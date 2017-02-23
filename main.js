330a7704423932105f7bf950dae74b9234cb9cea
Lourdes Vílchez <mlvilchezp@gmail.com>
2017-02-23 15:37:57 -0500

    Impresión en lista de la data del formulario

--- main.js
+++ main.js
@@ -6,6 +6,5 @@ var dato2 = document.getElementById("apellido").value;
 var dato3 = document.getElementById("dni").value;
 var dato4 = document.getElementById("direccion").value;
 
-document.getElementById("imprime").innerHTML = "<p>dato1</p>"
-
+document.getElementById("imprime").innerHTML = "<ul> <li> Nombre : " + dato1 + "</li> <li> Apellido : " + dato2 + " </li><li> DNI : " + dato3 + " </li> <li> Dirección : " + dato4 +"</li> </ul>";
 }
