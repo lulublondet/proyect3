c85733abc922dc05fb44107ff36752948c37bacc
Lourdes Vílchez <mlvilchezp@gmail.com>
2017-02-23 15:08:15 -0500

    creando la funcion de boton y presentando datos en el body

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
- 
+
+function mostrarDatos () {
+
+var dato1 = document.getElementById("nombre").value;
+var dato2 = document.getElementById("apellido").value;
+var dato3 = document.getElementById("dni").value;
+var dato4 = document.getElementById("direccion").value;
+
+document.getElementById("imprime").innerHTML = "<p>dato1</p>"
+
+}
